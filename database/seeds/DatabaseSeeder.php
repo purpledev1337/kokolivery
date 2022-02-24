ec2a14ca700925e4cae59733163f19bd066f386c
Maurizio <proiettomaurizio97@gmail.com>
2022-02-24 19:17:18 +0100

    resolve conflict

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,20 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // $this->call(OrderSeeder::class);
-=======
-=======
->>>>>>> 3dbd5f441bc649ef114d93e3fcfb5a3852b2a55b
+
         $this->call([
             UserSeeder::class,
             TypeSeeder::class,
         ]
         );
-<<<<<<< HEAD
->>>>>>> 3dbd5f441bc649ef114d93e3fcfb5a3852b2a55b
-=======
->>>>>>> 3dbd5f441bc649ef114d93e3fcfb5a3852b2a55b
     }
 }
