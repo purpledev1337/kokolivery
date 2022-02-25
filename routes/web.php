55b2b39e8cc6a8d985b2732955c15ca8daa66931
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2022-02-25 17:10:17 +0100

    add route login

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'GuestController@home')->name('home');
 
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
+
+Route::get('/register', 'GuestController@register') -> name('register');
+
+
 Route::middleware(['auth'])->prefix('post')->group(function () {
     // Route::get('/create', 'PostController@create') -> name('create');
     // Route::post('/store', 'PostController@store') -> name('store');
