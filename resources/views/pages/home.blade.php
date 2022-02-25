d6ef8b4377f52aed6e08e871d8878e11b49d3424
Maurizio Proietto <87990939+Maurizio97@users.noreply.github.com>
2022-02-25 16:20:29 +0100

    create adn set homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <title>test</title>
+</head>
+<body>
+
+    <div id="cont">
+        <div id="cont-logo">
+            <div id="cont-img">
+                <img id="float-img" src="asset/kokolivery-logo.svg" alt="">
+            </div>
+            <h1 id="float-title">
+                Kokolivery
+            </h1> 
+        </div> 
+        <div>
+            <input type="text" placeholder="Inserisci La Via, La Citta' E La Provincia">
+            <button class="btn btn-primary" >SEARCH</button>
+        </div>
+    </div> 
+
+
+</body>
+</html>
