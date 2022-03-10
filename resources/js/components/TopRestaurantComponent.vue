70d92b2596234d75f8f6f091f8c0651ca24dfe33
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2022-03-10 12:22:19 +0100

    cards complete

--- resources/js/components/TopRestaurantComponent.vue
+++ resources/js/components/TopRestaurantComponent.vue
@@ -1,22 +1,5 @@
 <template>
-    <!-- <div id="top-restaurant" class="container-fluid content my-5"> -->
-        <!-- <div class="row flex-nowrap overflow-auto row-cols-2 row-cols-lg-4 position-relative">
-            <div v-for="restaurant in topRestaurants" :key="restaurant.id" class="col">
-              <a :href="`restaurant/shop/${restaurant.id}`" class="card h-100 shadow bg-body roundedTop roundedBottom">
-                  <div class="card-img-top position-relative roundedTop">
-                    <img src="https://source.unsplash.com/random" class="roundedTop" alt="restaurant.brand_name">
-                    <h4 class="card-title position-absolute">{{restaurant.brand_name}}</h4>
-                  </div>
-                <div class="card-body">
-                  <p class="card-text">{{restaurant.description}}</p>
-                  <span>{{restaurant.rating}} <i class="fas fa-star"></i> ({{restaurant.num_rating}}) </span>
-                  <span>{{restaurant.city}}</span>
-                </div>
-              </a>
-            </div>
-        </div> -->
-        <card-component :filteredRestaurants="topRestaurants"></card-component>
-    <!-- </div> -->
+  <card-component :filteredRestaurants="topRestaurants"></card-component>
 </template>
 
 <script>
