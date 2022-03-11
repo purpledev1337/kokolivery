83c9de02f7daee9a9da5fb272af32ef6b9514456
Maurizio Proietto <87990939+Maurizio97@users.noreply.github.com>
2022-03-11 17:13:50 +0100

    code refactoring

--- resources/js/components/ChartComponent.vue
+++ resources/js/components/ChartComponent.vue
@@ -9,14 +9,9 @@
     import Chart from 'chart.js/auto';
 
     export default {
-        // props: {
-        //     labels: String,
-        //     dataProp: String,
-        //     colors: String,
-        // },
         data(){
             return{
-                // dataChart: null,
+
                 labels: null,
                 dataProp: null,
                 colors: null,
@@ -26,17 +21,11 @@
             async getDataChart(){
                 let res = await axios.get('/charts')
                 .catch(error => console.error(error));
-                // this.dataChart = res.data;
                 this.labels = res.data.labels;
                 this.dataProp = res.data.dataset;
                 this.colors = res.data.colours;
-                console.log(res.data);
-                console.log('labels',this.labels);
-                console.log('dataset',this.dataProp);
-                console.log('color',this.colors);
                 this.renderChart();
             },
-            // polarArea
             renderChart() {
                 new Chart(document.getElementById('canvas').getContext('2d'), {
                     type: 'bar',
@@ -59,9 +48,6 @@
             }
         },
         mounted() {
-            // console.log(JSON.parse(this.labels));
-            // console.log('data',JSON.parse(this.dataProp));
-            // this.renderChart();
             this.getDataChart();
         }
     }
--- resources/views/pages/auth/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main-layout')
-@section('content')
-<my-component
-    labels="{{ json_encode(['2014', '2015', '2016', '2017', '2020']) }}"
-    data-prop="{{ json_encode(['99', '66', '87', '49', '92']) }}">
-</my-component>
-@endsection
